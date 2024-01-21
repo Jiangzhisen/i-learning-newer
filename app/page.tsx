d973863be7621f70c3b72a31e0c2d4f32e10330b
Jiangzhisen <zzzz4444450@gmail.com>
2024-01-22 00:12:31 +0800

    -Add the shadcn-ui package.

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,9 @@
-import Image from 'next/image'
-import { PrismaClient } from '@prisma/client'
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-
-  const prisma = new PrismaClient()
-
-  async function main() {
-    const post = await prisma.post.update({
-      where: { id: 1 },
-      data: { published: true },
-    })
-    console.log(post)
-  }
-  
-  main()
-    .then(async () => {
-      await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-      console.error(e)
-      await prisma.$disconnect()
-      process.exit(1)
-  })
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World!!
+      <Button>Click me</Button>
     </main>
-  )
+  );
 }
